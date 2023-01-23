d5ec24af6e638cddb6d6a4fc6cce286b9ebc9e6a
JoakimKruse <kruse_95@live.se>
2023-01-23 16:16:39 +0100

    Added a new feature

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world! - change 6-7 combined");
+        System.out.println("Hello world! - This is a new feature");
     }
 }
